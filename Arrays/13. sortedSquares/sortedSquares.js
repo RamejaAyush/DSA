14552e0c9c6e3cacb343da0c372a3e338061005a
Ayush Rameja <ayushrwork@gmail.com>
2023-03-18 11:09:00 +0530

    Array: added sorted square array answer

--- Arrays/13. sortedSquares/sortedSquares.js
+++ Arrays/13. sortedSquares/sortedSquares.js
@@ -1,11 +1,22 @@
 const sortedSquares = (nums) => {
-  let start = 0
-  let end = nums.length - 1
-  let sortedIndx = end
-  let sortedArr = []
+  let min = 0,
+    max = nums.length - 1,
+    newArr = [],
+    indx = max
 
-  while (sortedIndx < -1) {
-    if (Math.abs(nums[start++]) < Math.abs(nums[end--])) {
+  while (indx > -1) {
+    console.log(Math.abs(nums[max]), Math.abs(nums[min]))
+    if (Math.abs(nums[max]) > Math.abs(nums[min])) {
+      newArr[indx--] = nums[max] * nums[max]
+      max--
+    } else {
+      newArr[indx--] = nums[min] * nums[min]
+      min++
     }
   }
+
+  console.log(newArr)
+  return newArr
 }
+
+sortedSquares([-4, -1, 0, 3, 10])
