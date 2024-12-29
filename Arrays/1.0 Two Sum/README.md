e8ef6f2d0e698684678be590a43cff81e6cdd7b6
Ayush Rameja <ayushrameja@Ayushs-MacBook-Pro.local>
2024-12-29 17:26:32 +0530

    forked main and converting the code to typescript

--- Arrays/1.0 Two Sum/README.md
+++
@@ -1,6 +0,0 @@
-# Two Sum
-
-to approach this problem, I'm using two pointer technique to find the sum and checking if that is equal to the target
-
-Space Complexity: O(n),
-Time Complexity: O(n^2)
--- Tree/Binary Tree/DFS and Depth of tree/depth of a tree.js
+++
@@ -1,9 +0,0 @@
-const depthTree = (root, count = 0) => {
-  if (!root) return count
-
-  count++
-  return Math.max(depthTree(root.left, count), depthTree(root.right, count))
-}
-
-// space complex: O(N)
-// Time complex: O(N)
